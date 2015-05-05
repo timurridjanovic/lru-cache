38689515e65636e4bc75b335ac149e180724dc3a
timurridjanovic <timur.ridjanovic@gmail.com>
2015-05-05 13:59:20 -0400

    first commit

---
+++ tests/lru_cache_scale_tests.py
@@ -0,0 +1,37 @@
+import unittest
+from nose.tools import raises
+
+from lru_cache import LRUCache
+
+class LRUCacheScaleTests(unittest.TestCase):
+    def setUp(self):
+        self.capacity = 500000
+        self.lru_cache = LRUCache(self.capacity)
+        
+        for i in xrange(1, self.capacity + 2):
+            self.lru_cache.set('user' + str(i), 'user_number_' + str(i))
+
+    def tearDown(self):
+        self.lru_cache = None
+
+    @raises(KeyError)
+    def test_set_and_get(self):
+        """
+        lru element should be user2 and user1 sould be removed.
+        """
+
+        self.assertEqual(self.lru_cache.get_lru_el(), self.lru_cache._cache_dict['user2'])
+        self.lru_cache.get('user1')
+
+    def test_update(self):
+        """
+        test update
+        """
+
+        self.lru_cache.set('user' + str(self.capacity/2), 'ANON_USER')
+        self.assertTrue(self.lru_cache.get('user' + str(self.capacity/2)) == 'ANON_USER')
+
+        self.lru_cache.set('user2', 'USER2')
+        self.assertEqual(self.lru_cache.get_lru_el(), self.lru_cache._cache_dict['user3'])
+
+
